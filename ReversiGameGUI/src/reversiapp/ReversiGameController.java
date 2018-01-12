be7133784043e93febb3fe18b4068b5755d0a549
Keren levy <kerenle2@hotmail.com>
2018-01-12 17:45:01 +0200

    add a menu

--- ReversiGameGUI/src/reversiapp/ReversiGameController.java
+++ ReversiGameGUI/src/reversiapp/ReversiGameController.java
@@ -4,12 +4,20 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.event.ActionEvent;
 
 public class ReversiGameController implements Initializable{
 	@FXML
-	private HBox root;
+	private AnchorPane root;
+	@FXML
+	private Button start;
 	private int board_size;	 
 	private int[][]board;		 
 	private Player[] players;
@@ -24,8 +32,19 @@ public class ReversiGameController implements Initializable{
 				this.board[i][j] = 0;
 			}
 		}
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ReversiGame.fxml"));
+		fxmlLoader.setRoot(this);
+		fxmlLoader.setController(this);
+		root.setOnMouseClicked(event-> {this.start(event);});
+
+//		root.setOnKeyPressed(reversiBoard.getOnKeyPressed());
+	
+		
+
+	}
+	@FXML
+	public void start(MouseEvent event){
 		ReversiBoardController reversiBoard = new ReversiBoardController(board);
-		root.setOnKeyPressed(reversiBoard.getOnKeyPressed());
 		reversiBoard.setPoint(new Point(board.length/2 , board.length/2 , 'O'));
 		reversiBoard.setPoint(new Point(board.length/2 +1 , board.length/2 + 1 , 'O'));
 		reversiBoard.setPoint(new Point(board.length/2 , board.length/2 +1 , 'X'));
@@ -33,12 +52,11 @@ public class ReversiGameController implements Initializable{
 
 		reversiBoard.setPrefWidth(400);
 		reversiBoard.setPrefHeight(400);
-		
-		root.getChildren().add(0, reversiBoard);
+		root.getChildren().add( reversiBoard);
 		reversiBoard.draw();
 		//to handle window resize
 		root.widthProperty().addListener((observable, oldValue, newValue) -> {
-			double boardNewWidth = newValue.doubleValue() - 120;
+			double boardNewWidth = newValue.doubleValue();
 			reversiBoard.setPrefWidth(boardNewWidth);
 			reversiBoard.draw();
 			});
@@ -47,5 +65,4 @@ public class ReversiGameController implements Initializable{
 				reversiBoard.draw();
 			});
 	}
-	
 }
